4fda669df30b28f42f59984b03ee8ae5bc12711f
4lch3my <fordit1517@gmail.com>
2021-05-16 04:30:56 +0200

    Update auto_uploader.bat

--- auto_uploader.bat
+++ auto_uploader.bat
@@ -4,8 +4,8 @@ PING  -n 1 "" | FIND "TTL="
 IF ERRORLEVEL 1 (SET OUT=4F  & echo Request timed out.) ELSE (SET OUT=2F)
 color %OUT%
 ping -n 2 -l 10 127.0.0.1 >nul
-echo File Copy Started
-xcopy  /Y "FROM_LOTION" "TO_LOCATION" 
-echo File Copy Completed
+echo File Upload Started...
+xcopy  /Y "FROM_LOCATION" "TO_LOCATION" 
+echo File Upload Completed!
 timeout 60
 GoTo top
